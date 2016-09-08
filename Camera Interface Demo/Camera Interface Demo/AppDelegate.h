21bb49a08f218d397736705045d5b539e6f97a44
Cian Duffy <cianduffyty@gmail.com>
2016-09-08 21:44:56 +0100

    Initial push. Empty Xcode project

---
+++ Camera Interface Demo/Camera Interface Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera Interface Demo
+//
+//  Created by Cian Duffy on 08/09/2016.
+//  Copyright © 2016 Cian Duffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Camera Interface Demo/Camera Interface Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera Interface Demo
+//
+//  Created by Cian Duffy on 08/09/2016.
+//  Copyright © 2016 Cian Duffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
